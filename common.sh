85199c9d7cdc8af4d549cf9451d1dc429638dba2
PradeepReddy927 <prem.pradeepreddy@gmail.com>
2025-11-03 07:00:08 +0530

    roboshop-common messages

--- common.sh
+++ common.sh
@@ -8,6 +8,7 @@ LOGS_FOLDER="/var/log/shell-roboshop"
 SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" # /var/log/shell-script/16-logs.log
 START_TIME=$(date +%s)
+SCRIPT_DIR=$PWD
 MONGODB_HOST=mongodb.dawsdevops86.fun
 
 mkdir -p $LOGS_FOLDER
@@ -40,7 +41,7 @@ nodejs_setup(){
 
     npm install &>>$LOG_FILE
     VALIDATE $? "Install dependencies"
-     
+
 }
 
 app_setup(){
